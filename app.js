d92b229a427b3cf4db6cc7b7068130bf5ae7f657
alaincr7 <alain.caro07@gmail.com>
2022-07-20 15:18:05 -0500

    se corrije mensaje

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/', (req, res)=>{
 
     console.log('peticion recibida')
 
-    res.send('<h1>ORGULLOSO DE SER COLOMBIANO -- 20 DE JULIO DIA DE LA INDEPENCIA DE COLOMBIA</h1>')
+    res.send('<h1>ORGULLOSO DE SER COLOMBIANO -- 20 DE JULIO DIA DE LA INDEPENDENCIA DE COLOMBIA</h1>')
 })
 
 const PORT = process.env.PORT || 4000
